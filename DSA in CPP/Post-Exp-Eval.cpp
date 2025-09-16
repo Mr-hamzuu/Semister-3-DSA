ce184e6bb8cab266b2097086ec96365bb9152198
Mr-hamzuu <hamzuu6971@gmail.com>
2025-09-16 19:28:46 +0500

    new file

---
+++ DSA in CPP/Post-Exp-Eval.cpp
@@ -0,0 +1,126 @@
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+
+// int main()
+// {
+//     stack<int> main;
+//     stack<int> min;
+
+//     int n[5]={6,3,8,4,2};
+//     for (int i = 0; i < 5; i++)
+//     {
+//         main.push(n[i]);
+//        if (min.empty()||n[i]<=min.top())
+//        {
+//         min.push(n[i]);
+//        }
+
+//     }
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int stacksize = 20;
+struct stack
+{
+    int arr[stacksize];
+    int top;
+};
+
+int top(stack &s)
+{
+    return s.top;
+}
+bool isfull(stack &s)
+{
+    if (s.top == stacksize - 1)
+    {
+        return true;
+    }
+    return false;
+}
+void push(stack &s, int value)
+{
+    if (isfull(s))
+    {
+        cout << "Stack is full" << endl;
+    }
+    else
+        s.arr[++s.top] = value;
+}
+bool isempty(stack &s)
+{
+    if (s.top != -1)
+    {
+        return false;
+    }
+    return true;
+}
+int pop(stack &s)
+{
+    if (!isempty(s))
+    {
+        return s.arr[s.top--];
+    }
+    return 0;
+}
+int main()
+{
+    stack s;
+    s.top=-1;
+    string str = "623+-382/+*2$3+";
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            int symbol = str[i] - '0';
+            push(s, symbol);
+        }
+        else
+        {
+            int op2 = pop(s);
+            int op1 = pop(s);
+            int ans;
+            switch (str[i])
+            {
+            case '+':
+
+                 ans = op1 + op2;
+                push(s, ans);
+                break;
+            case '-':
+                
+                 ans = op1 - op2;
+                push(s, ans);
+                break;
+            case '*':
+                
+                 ans = op1 * op2;
+                push(s, ans);
+                break;
+            case '/':
+              
+                 ans = op1 / op2;
+                push(s, ans);
+                break;
+            case '$':
+               
+                 ans = pow(op1,op2);
+                push(s, ans);
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+cout<<"Result : "<<pop(s)<<endl;
+
+    return 0;
+}
